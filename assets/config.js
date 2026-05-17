ed9d3a368a6edefe72b0ee93aa1cb8008a0ff099
mlkphotography <mlkphotography555@gmail.com>
2026-05-17 14:47:57 +0800

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -5,7 +5,7 @@
   3. Change the WhatsApp number to your main business number.
 */
 window.MLK_CONFIG = {
-  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxmGrsOSJAqD9-PURt0-xzfTsgug-F0udQ01UTGKE5k85ootp7ejZ0wT26NXf5aqirm/exec',
+  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyjzFy7M7UiVZNLfTEY2fGQJ1u55a9JvIXlQrVWaqxCh6uwpZg1KzgVWEoI6jDZPv1Kkw/exec',
   WHATSAPP_NUMBER: '60162507723',
   GOOGLE_SHEET_URL: 'https://docs.google.com/spreadsheets/d/1McbqqN26ebA_lgsGk1cjPYJNIuHXbXJ7lbfqZvArV9Q/edit?pli=1&gid=2078324413#gid=2078324413',
   ADMIN_TOKEN: 'mlk-admin-2026'
